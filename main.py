8ae2ff52ea80a268f91c7e5063dc04bc3f1569db
pp2li <peipei.li@cripac.ia.ac.cn>
2020-08-12 22:18:54 +0800

    update1 1111

--- main.py
+++ main.py
@@ -12,6 +12,6 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
-    a = 10
+    ###a = 10
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
