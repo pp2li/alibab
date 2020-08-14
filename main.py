ff3d805a9e84495866162e91b1cc2e4df30d427f
pp2li <47687795+pp2li@users.noreply.github.com>
2020-08-14 17:43:19 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,2 @@
-class Solution(object):
-    def game(self, guess, answer):
-        """
-        :type guess: List[int]
-        :type answer: List[int]
-        :rtype: int
-        """
-        m=0
-        count = 0
-        while m<3:
-            if guess[m] == answer[m]:
-                count+=1
-            m+=1
-        return count
+def test(nums1):
+    print('a')
