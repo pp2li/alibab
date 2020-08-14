e8e4381a0f01fa0498aa629b68b42d5420474bad
pp2li <peipei.li@cripac.ia.ac.cn>
2020-08-14 17:34:45 +0800

    update1 1111

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-
+class Solution(object):
+    def game(self, guess, answer):
+        """
+        :type guess: List[int]
+        :type answer: List[int]
+        :rtype: int
+        """
+        m=0
+        count = 0
+        while m<3:
+            if guess[m] == answer[m]:
+                count+=1
+            m+=1
+        return count
